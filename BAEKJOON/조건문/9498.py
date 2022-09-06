3413213868a98c39c4c74965b906ac8e66e8d2b3
Chaewon Park <66204790+Pchaewon@users.noreply.github.com>
2022-09-06 14:20:57 +0900

    Add files via upload

---
+++ BAEKJOON/조건문/9498.py
@@ -0,0 +1,26 @@
+# x = int(input())
+
+# if x>=90 and x<=100:
+#     print("A")
+# elif x>=80 and x<=89:
+#     print("B")
+# elif x>=70 and x<=79:
+#     print("c")
+# elif x>=60 and x<=69:
+#     print("D")
+# else:
+#     print("F")
+    
+# score = int(input())
+
+# if score >= 90 :
+#     print('A')
+# elif score >= 80 :
+#     print('B')
+# elif score >= 70 :
+#     print('C')
+# elif score >= 60 :
+#     print('D')
+# else:
+#     print('F')
+    
