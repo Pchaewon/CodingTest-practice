a48819e4a59e9548aec882d31545eca39d35ec4e
Chaewon Park <66204790+Pchaewon@users.noreply.github.com>
2022-09-06 14:21:46 +0900

    Add files via upload

---
+++ BAEKJOON/함수/15596.py
@@ -0,0 +1,13 @@
+# sum 함수
+def solve(a):
+    return sum(a)
+
+# # for 문
+# def solve(a):
+#     total = 0
+#     for x in a:
+#         total += x
+#     return total
+
+a = map(int, input().split())
+solve(a)
