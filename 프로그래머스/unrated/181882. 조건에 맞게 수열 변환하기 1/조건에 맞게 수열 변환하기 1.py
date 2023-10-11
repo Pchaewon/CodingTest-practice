2c288c160182198e7051cd654142ba61e5f614fb
Chaewon Park <66204790+Pchaewon@users.noreply.github.com>
2023-10-11 17:25:27 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 22.13 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    answer = []
+    
+    for a in arr:
+        if a>=50 and a%2==0:
+            answer.append(a//2)
+        elif a<50 and a%2!=0:
+            answer.append(a*2)
+        else:
+            answer.append(a)
+            
+    return answer
